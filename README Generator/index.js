a6c9065843528470469b5d27d8d79728ac422dcc
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-08-17 22:24:32 -0500

    added sections in inquirer for description, install instructions, usage information, conbribution guildelines and test instructions added validation for each of the sections just created

--- README Generator/index.js
+++ README Generator/index.js
@@ -10,7 +10,67 @@ const questions =
             {
                 type: 'input',
                 message: 'What is the title of your project?',
-                name: 'projTitle',
+                name: 'title',
+                validate: (value) =>{
+                    if(value){
+                        return true
+                    } else{
+                        return 'Required field left blank, please enter a value.'
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: 'What is the description of your project?',
+                name: 'description',
+                validate: (value) =>{
+                    if(value){
+                        return true
+                    } else{
+                        return 'Required field left blank, please enter a value.'
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: 'How do you install your application?',
+                name: 'install',
+                validate: (value) =>{
+                    if(value){
+                        return true
+                    } else{
+                        return 'Required field left blank, please enter a value.'
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: 'Usage information?', //what does this mean?
+                name: 'usage',
+                validate: (value) =>{
+                    if(value){
+                        return true
+                    } else{
+                        return 'Required field left blank, please enter a value.'
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: 'Who are the contributors for this project?',
+                name: 'contributors',
+                validate: (value) =>{
+                    if(value){
+                        return true
+                    } else{
+                        return 'Required field left blank, please enter a value.'
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: 'Test?', //what does this mean?
+                name: 'tests',
                 validate: (value) =>{
                     if(value){
                         return true
@@ -20,14 +80,13 @@ const questions =
                 }
             },
         ])
-
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
-.then((data) => {
-    fs.writeFile('README.md', JSON.stringify(data) ,(err) =>
-        err ? console.error(err) : console.log('Success! Your README file has been generated.')
-    );
-});
+// .then((data) => {
+//     fs.writeFile('README.md', JSON.stringify(data) ,(err) =>
+//         err ? console.error(err) : console.log('Success! Your README file has been generated.')
+//     );
+// });
 
 // TODO: Create a function to initialize app
 // function init() {}

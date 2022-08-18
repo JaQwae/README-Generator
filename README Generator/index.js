3456b2b2528b045b9a713f0badf10fdbcd3c3460
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-08-17 21:57:56 -0500

    encoded validationon title that does not allow the user proceed if a value is not inputed

--- README Generator/index.js
+++ README Generator/index.js
@@ -4,20 +4,28 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the title of your project?',
-            name: 'projTitle',
-        },
-    ])
+const questions = 
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the title of your project?',
+                name: 'projTitle',
+                validate: (value) =>{
+                    if(value){
+                        return true
+                    } else{
+                        return 'Required field left blank, please enter a value.'
+                    }
+                }
+            },
+        ])
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
 .then((data) => {
     fs.writeFile('README.md', JSON.stringify(data) ,(err) =>
-        err ? console.error(err) : console.log('Success!')
+        err ? console.error(err) : console.log('Success! Your README file has been generated.')
     );
 });
 

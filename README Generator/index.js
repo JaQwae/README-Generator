468d47ad9c5fbe6e051cf0c1bb4c59dac7ba3b4f
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-08-20 12:28:30 -0500

    readme file generated as a line object

--- README Generator/index.js
+++ README Generator/index.js
@@ -1,18 +1,18 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown.js')
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = 
+const questions =  //do we need to set this to a variable
     inquirer
         .prompt([
             {
                 type: 'input',
                 message: 'What is the title of your project?',
                 name: 'title',
-                validate: (value) =>{
-                    if(value){
+                validate: (title) =>{
+                    if(title){
                         return true
                     } else{
                         return 'Required field left blank, please enter a value.'
@@ -23,8 +23,8 @@ const questions =
                 type: 'input',
                 message: 'What is the description of your project?',
                 name: 'description',
-                validate: (value) =>{
-                    if(value){
+                validate: (description) =>{
+                    if(description){
                         return true
                     } else{
                         return 'Required field left blank, please enter a value.'
@@ -35,8 +35,8 @@ const questions =
                 type: 'input',
                 message: 'How do you install your application?',
                 name: 'install',
-                validate: (value) =>{
-                    if(value){
+                validate: (install) =>{
+                    if(install){
                         return true
                     } else{
                         return 'Required field left blank, please enter a value.'
@@ -47,8 +47,8 @@ const questions =
                 type: 'input',
                 message: 'Usage information?', //what does this mean?
                 name: 'usage',
-                validate: (value) =>{
-                    if(value){
+                validate: (usage) =>{
+                    if(usage){
                         return true
                     } else{
                         return 'Required field left blank, please enter a value.'
@@ -59,8 +59,8 @@ const questions =
                 type: 'input',
                 message: 'Who are the contributors for this project?',
                 name: 'contributors',
-                validate: (value) =>{
-                    if(value){
+                validate: (contributors) =>{
+                    if(contributors){
                         return true
                     } else{
                         return 'Required field left blank, please enter a value.'
@@ -71,8 +71,8 @@ const questions =
                 type: 'input',
                 message: 'Test?', //what does this mean?
                 name: 'tests',
-                validate: (value) =>{
-                    if(value){
+                validate: (tests) =>{ //Do we need to require this section?
+                    if(tests){
                         return true
                     } else{
                         return 'Required field left blank, please enter a value.'
@@ -82,15 +82,17 @@ const questions =
         ])
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
-// .then((data) => {
-//     fs.writeFile('README.md', JSON.stringify(data) ,(err) =>
-//         err ? console.error(err) : console.log('Success! Your README file has been generated.')
-//     );
-// });
+
+.then((data) => {
+    fs.writeFile('README.md', JSON.stringify(data) ,(err) =>
+        err ? console.error(err) : console.log('Success! Your README file has been generated.')
+    );
+});
+
+// console.log(questions.ui.answers)
 
 // TODO: Create a function to initialize app
 // function init() {}
 
 // Function call to initialize app
 // init();
-// console.log(generateMarkdown)

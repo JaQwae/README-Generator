6ab77c3c3bfe9e8fa37e3262b13bb960f4604d32
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-08-20 15:15:06 -0500

    change question format to just an array change structure of init function includes the inquirer npm prompt

--- README Generator/index.js
+++ README Generator/index.js
@@ -4,7 +4,81 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
-var questions; 
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+        validate: (title) =>{
+            if(title){
+                return true
+            } else{
+                return 'Required field left blank, please enter a value.'
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'What is the description of your project?',
+        name: 'description',
+        validate: (description) =>{
+            if(description){
+                return true
+            } else{
+                return 'Required field left blank, please enter a value.'
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'How do you install your application?',
+        name: 'install',
+        validate: (install) =>{
+            if(install){
+                return true
+            } else{
+                return 'Required field left blank, please enter a value.'
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Usage information?', //what does this mean?
+        name: 'usage',
+        validate: (usage) =>{
+            if(usage){
+                return true
+            } else{
+                return 'Required field left blank, please enter a value.'
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Who are the contributors for this project?',
+        name: 'contributors',
+        validate: (contributors) =>{
+            if(contributors){
+                return true
+            } else{
+                return 'Required field left blank, please enter a value.'
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Test?', //what does this mean?
+        name: 'tests',
+        validate: (tests) =>{ //Do we need to require this section?
+            if(tests){
+                return true
+            } else{
+                return 'Required field left blank, please enter a value.'
+            }
+        }
+    },
+]
 
 
 // TODO: Create a function to write README file
@@ -16,92 +90,16 @@ var questions;
 //     );
 // });
 
-// Function call to initialize app
-init();
-
 // Create a function to initialize app
 function init() {
-    // TODO: Create an array of questions for user input
-    const questions = inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the title of your project?',
-            name: 'title',
-            validate: (title) =>{
-                if(title){
-                    return true
-                } else{
-                    return 'Required field left blank, please enter a value.'
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'What is the description of your project?',
-            name: 'description',
-            validate: (description) =>{
-                if(description){
-                    return true
-                } else{
-                    return 'Required field left blank, please enter a value.'
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'How do you install your application?',
-            name: 'install',
-            validate: (install) =>{
-                if(install){
-                    return true
-                } else{
-                    return 'Required field left blank, please enter a value.'
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'Usage information?', //what does this mean?
-            name: 'usage',
-            validate: (usage) =>{
-                if(usage){
-                    return true
-                } else{
-                    return 'Required field left blank, please enter a value.'
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'Who are the contributors for this project?',
-            name: 'contributors',
-            validate: (contributors) =>{
-                if(contributors){
-                    return true
-                } else{
-                    return 'Required field left blank, please enter a value.'
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'Test?', //what does this mean?
-            name: 'tests',
-            validate: (tests) =>{ //Do we need to require this section?
-                if(tests){
-                    return true
-                } else{
-                    return 'Required field left blank, please enter a value.'
-                }
-            }
-        },
-    ])
-
-    .then(answers => {
-        console.log(answers)
+    inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers);
     });
 }
+
+// Function call to initialize app
+init();
 
 //exports
 module.exports = questions;

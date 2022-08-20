e0b55bbd073be0d28911a49876f4372231246db7
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-08-20 15:03:26 -0500

    init() is returning answer values

--- README Generator/index.js
+++ README Generator/index.js
@@ -3,98 +3,9 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions =  //do we need to set this to a variable
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'What is the title of your project?',
-                name: 'title',
-                validate: (title) =>{
-                    if(title){
-                        return true
-                    } else{
-                        return 'Required field left blank, please enter a value.'
-                    }
-                }
-            },
-            {
-                type: 'input',
-                message: 'What is the description of your project?',
-                name: 'description',
-                validate: (description) =>{
-                    if(description){
-                        return true
-                    } else{
-                        return 'Required field left blank, please enter a value.'
-                    }
-                }
-            },
-            {
-                type: 'input',
-                message: 'How do you install your application?',
-                name: 'install',
-                validate: (install) =>{
-                    if(install){
-                        return true
-                    } else{
-                        return 'Required field left blank, please enter a value.'
-                    }
-                }
-            },
-            {
-                type: 'input',
-                message: 'Usage information?', //what does this mean?
-                name: 'usage',
-                validate: (usage) =>{
-                    if(usage){
-                        return true
-                    } else{
-                        return 'Required field left blank, please enter a value.'
-                    }
-                }
-            },
-            {
-                type: 'input',
-                message: 'Who are the contributors for this project?',
-                name: 'contributors',
-                validate: (contributors) =>{
-                    if(contributors){
-                        return true
-                    } else{
-                        return 'Required field left blank, please enter a value.'
-                    }
-                }
-            },
-            {
-                type: 'input',
-                message: 'Test?', //what does this mean?
-                name: 'tests',
-                validate: (tests) =>{ //Do we need to require this section?
-                    if(tests){
-                        return true
-                    } else{
-                        return 'Required field left blank, please enter a value.'
-                    }
-                }
-            },
-        ])
 
-.then(answers => {
+var questions; 
 
-    console.log(answers.title);
-
-    console.log(answers.description);
-
-    console.log(answers.install);
-
-    console.log(answers.usage);
-
-    console.log(answers.contributors);
-
-    console.log(answers.tests);
-});
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
@@ -105,16 +16,92 @@ const questions =  //do we need to set this to a variable
 //     );
 // });
 
-// Create a function to initialize app
-function init() {
-    inquirer.prompt(questions)
-        .then(function(answers) {
-            console.log(answers);
-    });
-}
-
 // Function call to initialize app
 init();
+
+// Create a function to initialize app
+function init() {
+    // TODO: Create an array of questions for user input
+    const questions = inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+            validate: (title) =>{
+                if(title){
+                    return true
+                } else{
+                    return 'Required field left blank, please enter a value.'
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is the description of your project?',
+            name: 'description',
+            validate: (description) =>{
+                if(description){
+                    return true
+                } else{
+                    return 'Required field left blank, please enter a value.'
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'How do you install your application?',
+            name: 'install',
+            validate: (install) =>{
+                if(install){
+                    return true
+                } else{
+                    return 'Required field left blank, please enter a value.'
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'Usage information?', //what does this mean?
+            name: 'usage',
+            validate: (usage) =>{
+                if(usage){
+                    return true
+                } else{
+                    return 'Required field left blank, please enter a value.'
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'Who are the contributors for this project?',
+            name: 'contributors',
+            validate: (contributors) =>{
+                if(contributors){
+                    return true
+                } else{
+                    return 'Required field left blank, please enter a value.'
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'Test?', //what does this mean?
+            name: 'tests',
+            validate: (tests) =>{ //Do we need to require this section?
+                if(tests){
+                    return true
+                } else{
+                    return 'Required field left blank, please enter a value.'
+                }
+            }
+        },
+    ])
+
+    .then(answers => {
+        console.log(answers)
+    });
+}
 
 //exports
 module.exports = questions;
--- README Generator/utils/generateMarkdown.js
+++ README Generator/utils/generateMarkdown.js
@@ -17,7 +17,13 @@ const index = require('../index'); //why two dots here
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
-  return `# ${answers.title}`;
+  return `
+  # ${answers.title}`;
+  console.log(answers.description);
+  console.log(answers.install);
+  console.log(answers.usage);
+  console.log(answers.contributors);
+  console.log(answers.tests); 
 }
 
 // export
